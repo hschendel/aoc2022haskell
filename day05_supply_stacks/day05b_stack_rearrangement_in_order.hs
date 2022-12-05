ffcb0abb505a539255c5ccdc41e45c7c250880bf
Hagen Schendel <mail@hschendel.de>
2022-12-05 10:57:37 +0100

    refactor: remove tail recursion from day 5

--- day05_supply_stacks/day05b_stack_rearrangement_in_order.hs
+++ day05_supply_stacks/day05b_stack_rearrangement_in_order.hs
@@ -1,20 +1,20 @@
 import Data.List
 
-parseStacksLine [] (' ':'1':_) = []
-parseStacksLine a "" = a
-parseStacksLine a ('[':x:']':l) = parseStacksLine (a ++ [Just x]) (drop 1 l)
-parseStacksLine a (' ':' ':' ':l) = parseStacksLine (a ++ [Nothing]) (drop 1 l)
+parseStacksLine (' ':'1':_) = []
+parseStacksLine "" = []
+parseStacksLine ('[':x:']':l) = (Just x) : parseStacksLine (drop 1 l)
+parseStacksLine (' ':' ':' ':l) = Nothing : parseStacksLine (drop 1 l)
 
-appendStacksLine doneStacks stacks [] = doneStacks ++ stacks
-appendStacksLine doneStacks [] (Nothing:entries) = appendStacksLine (doneStacks ++ [[]]) [] entries
-appendStacksLine doneStacks (stack:stacks) (Nothing:entries) = appendStacksLine (doneStacks ++ [stack]) stacks entries
-appendStacksLine doneStacks [] ((Just x):entries) = appendStacksLine (doneStacks ++ [[x]]) [] entries
-appendStacksLine doneStacks (stack:stacks) ((Just x):entries) = appendStacksLine (doneStacks ++ [stack ++ [x]]) stacks entries
+appendStacksLine stacks [] = stacks
+appendStacksLine [] (Nothing:entries) = [] : (appendStacksLine [] entries)
+appendStacksLine (stack:stacks) (Nothing:entries) = stack : (appendStacksLine stacks entries)
+appendStacksLine [] ((Just x):entries) = [x] : (appendStacksLine [] entries)
+appendStacksLine (stack:stacks) ((Just x):entries) = (stack ++ [x]) : (appendStacksLine stacks entries)
 
 parseStacks stacks (l:ls) =
-    let entries = parseStacksLine [] l in
+    let entries = parseStacksLine l in
     if entries == [] then (stacks, (drop 1 ls))
-    else parseStacks (appendStacksLine [] stacks entries) ls
+    else parseStacks (appendStacksLine stacks entries) ls
 
 isDigit c = '0' <= c && c <= '9'
 
@@ -40,29 +40,29 @@ filterMoveLines ((Just move):ls) = move : (filterMoveLines ls)
 
 parseMoveLines ls = filterMoveLines (map parseMoveLine ls)
 
-applyMovePickUp 0 count doneStacks (stack:stacks) = (doneStacks ++ [drop count stack] ++ stacks, take count stack)
-applyMovePickUp from count doneStacks (stack:stacks) =
-    applyMovePickUp (from-1) count (doneStacks ++ [stack]) stacks
+applyMovePickUp 0 count (stack:stacks) = ([drop count stack] ++ stacks, take count stack)
+applyMovePickUp from count (stack:stacks) =
+    let (stacks2, pickedUp) = applyMovePickUp (from-1) count stacks in
+    (stack:stacks2, pickedUp)
 
-applyMovePutDown 0 movedElems doneStacks (stack:stacks) = (doneStacks ++ [movedElems ++ stack] ++ stacks)
-applyMovePutDown to movedElems doneStacks (stack:stacks) =
-    applyMovePutDown (to-1) movedElems (doneStacks ++ [stack]) stacks
+applyMovePutDown 0 movedElems (stack:stacks) = (movedElems ++ stack) : stacks
+applyMovePutDown to movedElems (stack:stacks) = stack : (applyMovePutDown (to-1) movedElems stacks)
 
 applyMove (count, from, to) stacks =
-    let (stacks2, movedElems) = applyMovePickUp from count [] stacks in
-    applyMovePutDown to movedElems [] stacks2
+    let (stacks2, movedElems) = applyMovePickUp from count stacks in
+    applyMovePutDown to movedElems stacks2
 
 applyMoves stacks [] = stacks
 applyMoves stacks (move:moves) = applyMoves (applyMove move stacks) moves
 
-topOfStacks seen [] = seen
-topOfStacks seen ([]:stacks) = topOfStacks (seen ++ " ") stacks
-topOfStacks seen ((top:stack):stacks) = topOfStacks (seen ++ [top]) stacks
+topOfStacks [] = []
+topOfStacks ([]:stacks) = ' ' : (topOfStacks stacks)
+topOfStacks ((top:stack):stacks) = top : (topOfStacks stacks)
 
 calc f =
     let (initialStacks, ls) = parseStacks [] (lines f) in
     let finalStacks = applyMoves initialStacks (parseMoveLines ls) in
-    topOfStacks "" finalStacks
+    topOfStacks finalStacks
 
 main = do
     f <- readFile "input.txt"
